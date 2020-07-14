2cf84d1ce5d782a5119aba56d58c75b3e32f77b0
TSephanya Brun <tsbrun@icloud.com>
2020-07-14 21:18:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if !phone.match(/\(?\d{3}\s?\)?\d{3}\s?-?\d{4}/)
-    return false
-  else
-    return true
-  end
+  !phone.match(/\(?\d{3}\s?\)?\d{3}\s?-?\d{4}/) ? false : true
 end
