09244088b6210e838a2a0cac6d3a8e12039e2e9c
TSephanya Brun <tsbrun@icloud.com>
2020-07-14 21:16:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if !phone.match(/\(?\d{3}\s?\)?\d{3}\s?-?\d{4}/)
+    return false
+  else
+    return true
+  end
 end
